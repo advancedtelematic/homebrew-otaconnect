9a7cefb65858d7e577d3c949cd79c78f7b1d95ee
Jon Oster <jon.oster@here.com>
2019-09-16 21:28:41 +0200

    Bump aktualizr to 2019.8
    Signed-off-by: Jon Oster <jon.oster@here.com>

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,7 +1,7 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage ""
-  version "2019.7"
+  version "2019.8"
   url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}"
   sha256 "782fa343c85be455d6e51bd774f3244e0dad093989ac9bb1d96215785f7e7314"
 
@@ -10,14 +10,15 @@ class Aktualizr < Formula
   depends_on "cmake" => :build
   depends_on "libarchive" => :build
   depends_on "libsodium" => :build
+  depends_on "curl-openssl" => :build
   depends_on "pkgconfig" => :build
   depends_on "openssl" => :build
   depends_on "python3" => :build
 
   bottle do
-    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2019.7"
+    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2019.8"
     cellar :any
-    sha256 "a39fb3c9d84916a9d8b8c699a7cf681f8465245cfb3fc7c74f04f877b040617b" => :mojave
+    sha256 "fc2941ce6d5f3389b0973ebf99b79f2ad7de331a3e9c4b999ded4b0df08392ec" => :mojave
   end
 
   def install

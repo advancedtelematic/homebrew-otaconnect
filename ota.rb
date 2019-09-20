babb0c738713200b9c92b063405bc2f9cb5f1123
Jon Oster <jon.oster@here.com>
2019-09-20 17:50:51 +0200

    Use tagged release, remove src cache and artifacts from install package
    Signed-off-by: Jon Oster <jon.oster@here.com>

--- ota.rb
+++ ota.rb
@@ -1,15 +1,14 @@
 class Ota < Formula
   desc "Manage OTA Connect devices, groups, packages and campaigns"
   homepage ""
-  url "https://github.com/advancedtelematic/ota-cli.git", :using => :git
   version "0.1.0"
+  url "https://github.com/advancedtelematic/ota-cli.git", :using => :git, :tag => "#{version}"
   sha256 "12042bdaa88299e9825681c28589337045116f74"
 
   depends_on "rust" => :build
 
   def install
-    ENV["CARGO_HOME"] = "#{prefix}"
-    system "make", "ota"
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do

e7e32095c5b083d24e08298ccf96312d99149b7d
Jon Oster <jon.oster@here.com>
2019-09-20 18:17:31 +0200

    Add bottle for ota-cli formula
    Signed-off-by: Jon Oster <jon.oster@here.com>

--- ota.rb
+++ ota.rb
@@ -1,5 +1,11 @@
 class Ota < Formula
   desc "Manage OTA Connect devices, groups, packages and campaigns"
+  bottle do
+    root_url "https://github.com/advancedtelematic/ota-cli/releases/download/0.1.0/"
+    cellar :any
+    sha256 "76d0598ea37213ac6b3741a00dfc421533cfd270a4174c5b4f37521cec6dc2be" => :mojave
+  end
+
   homepage ""
   version "0.1.0"
   url "https://github.com/advancedtelematic/ota-cli.git", :using => :git, :tag => "#{version}"

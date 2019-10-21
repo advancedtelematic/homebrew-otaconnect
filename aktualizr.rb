d094c4c36281d1ebc0f0c19ab7502d6937dee337
Jon Oster <jon.oster@here.com>
2019-10-21 10:08:34 +0200

    Update aktualizr recipe to 2019.9
    Signed-off-by: Jon Oster <jon.oster@here.com>

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,7 +1,7 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage ""
-  version "2019.8"
+  version "2019.9"
   url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}"
   sha256 "782fa343c85be455d6e51bd774f3244e0dad093989ac9bb1d96215785f7e7314"
 
@@ -16,9 +16,9 @@ class Aktualizr < Formula
   depends_on "python3" => :build
 
   bottle do
-    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2019.8"
+    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2019.9"
     cellar :any
-    sha256 "fc2941ce6d5f3389b0973ebf99b79f2ad7de331a3e9c4b999ded4b0df08392ec" => :mojave
+    sha256 "021255fc3b2b7409ab649adeb0abdb71efec564519ca7d0a6ad36d92abc9f8b6" => :mojave
   end
 
   def install

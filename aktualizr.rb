d452854ef0c2686fd7db0a43b05e3a273f7a86a9
Jon Oster <tkfu@users.noreply.github.com>
2020-02-13 17:05:07 +0100

    Revert "OTA-4241: fix aktualizr brewing"

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,32 +1,30 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage ""
-  version = "2020.2"
+  version "2019.9"
   url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}"
   sha256 "782fa343c85be455d6e51bd774f3244e0dad093989ac9bb1d96215785f7e7314"
 
-  depends_on "openssl@1.1"
-  depends_on "curl-openssl"
   depends_on "asn1c"
   depends_on "boost"
   depends_on "cmake" => :build
   depends_on "libarchive"
   depends_on "libsodium"
+  depends_on "curl-openssl"
   depends_on "pkgconfig" => :build
+  depends_on "openssl"
   depends_on "python3" => :build
 
   bottle do
-    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/#{version}"
+    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2019.9"
     cellar :any
-    sha256 "021255fc3b2b7409ab649adeb0abdb71efec564519ca7d0a6ad36d92abc9f8b6" => :catalina
+    sha256 "021255fc3b2b7409ab649adeb0abdb71efec564519ca7d0a6ad36d92abc9f8b6" => :mojave
   end
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DBoost_USE_MULTITHREADED=ON", "-DOPENSSL_SSL_LIBRARY=/usr/local/opt/openssl@1.1/lib/libssl.dylib", "-DOPENSSL_CRYPTO_LIBRARY=/usr/local/opt/openssl@1.1/lib/libcrypto.dylib", *std_cmake_args
-
-      system "make",  "-j#{ENV.make_jobs}"
-
+      system "cmake", "..", *std_cmake_args
+      system "make"
       system "mkdir", "-p", "#{bin}"
       system "cp", "./src/aktualizr_primary/aktualizr", "#{bin}/aktualizr"
     end

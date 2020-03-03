adba26ed2867409bacf9db8b4527a15f19c7a1ff
GitLab User <gitlab@C02MV1LJFD59.ad.here.com>
2020-03-03 15:34:57 +0100

    aktualizr 2020.3

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,31 +1,29 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage "https://github.com/advancedtelematic/aktualizr.git"
-
-  head "https://github.com/advancedtelematic/aktualizr.git"
-
   version = "2020.3"
-
-  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}"
+  
+  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}", :revision => "9f2cc5d7b026eb1a1404f0c77ef6cb9377245423"
+  head "https://github.com/advancedtelematic/aktualizr.git"
 
   # in case of --HEAD brewing the global version attribute will be equal to HEAD-<short-latest-commit-hash-of-master>
   # in case of stable/default brewing the global version will be equal to the latest release tag
-
-  depends_on "openssl@1.1"
-  depends_on "curl-openssl"
-  depends_on "asn1c"
-  depends_on "boost"
-  depends_on "cmake" => :build
-  depends_on "libarchive"
-  depends_on "libsodium"
-  depends_on "pkgconfig" => :build
-  depends_on "python3" => :build
-
+  
   bottle do
     root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2020.3"
     cellar :any
     sha256 "f42c799f7262f49a6debe2435254e14ef6bcc39b6168541a1fb09dffa6882f82" => :mojave
   end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python" => :build
+  depends_on "asn1c"
+  depends_on "boost"
+  depends_on "curl-openssl"
+  depends_on "libarchive"
+  depends_on "libsodium"
+  depends_on "openssl@1.1"
 
   def install
     args = %W[

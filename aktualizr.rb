ab3a85da4ad72079fff3a0bc9907a09c80663d30
GitLab User <gitlab@C02MV1LJFD59.ad.here.com>
2020-03-03 16:28:01 +0100

    aktualizr 2020.4

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,18 +1,18 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage "https://github.com/advancedtelematic/aktualizr.git"
-  version = "2020.3"
+  version = "2020.4"
 
-  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}", :revision => "9f2cc5d7b026eb1a1404f0c77ef6cb9377245423"
+  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}", :revision => "deea9a4562e88c466a3b203a62891bdd9d558af3"
   head "https://github.com/advancedtelematic/aktualizr.git"
 
   # in case of --HEAD brewing the global version attribute will be equal to HEAD-<short-latest-commit-hash-of-master>
   # in case of stable/default brewing the global version will be equal to the latest release tag
 
   bottle do
-    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2020.3"
+    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2020.4"
     cellar :any
-    sha256 "f42c799f7262f49a6debe2435254e14ef6bcc39b6168541a1fb09dffa6882f82" => :mojave
+    sha256 "7e0a533908a79af650702a14ddcaedaff410294edaa4edc1ff0febb20baa8ace" => :mojave
   end
 
   depends_on "cmake" => :build

b9120d5188d1a8a070af8ab7f4275c828ec0d883
GitLab User <gitlab@C02MV1LJFD59.ad.here.com>
2020-03-25 14:03:08 +0100

    aktualizr 2020.5

--- aktualizr.rb
+++ aktualizr.rb
@@ -1,16 +1,17 @@
 class Aktualizr < Formula
   desc "C++ Client for HERE OTA Connect"
   homepage "https://github.com/advancedtelematic/aktualizr.git"
-  version = "2020.4"
+  version = "2020.5"
+  revision = "9190ba20ba5bfda7cbf0aeb972deaf7e656dca0b"
 
-  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}", :revision => "f90e8996e826d130976a7b7f1835947b7e631025"
+  url "https://github.com/advancedtelematic/aktualizr.git", :using => :git, :tag => "#{version}", :revision => "#{revision}"
   head "https://github.com/advancedtelematic/aktualizr.git"
 
   # in case of --HEAD brewing the global version attribute will be equal to HEAD-<short-latest-commit-hash-of-master>
   # in case of stable/default brewing the global version will be equal to the latest release tag
 
   bottle do
-    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2020.4"
+    root_url "https://github.com/advancedtelematic/aktualizr/releases/download/2020.5"
     cellar :any
     sha256 "f42c799f7262f49a6debe2435254e14ef6bcc39b6168541a1fb09dffa6882f82" => :mojave
   end
